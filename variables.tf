d68cc14e6f72b33699403e30bc054e10ffeea1fd
Sivakumar Reddy M <info@joindevops.com>
2026-03-15 01:27:05 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "component" {
+    type = string
+}
+
+/* variable "health_check_path" {
+    default = "/health"
+} */
+
+/* variable "port_number" {
+    default = 8080
+} */
+
+variable "rule_priority" {
+    
+}
+
+variable "component" {
+
+}
+
+variable "project" {
+
+}
+
+variable "environment" {
+    
+}
